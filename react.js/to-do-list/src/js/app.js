32fe481a3a7ed7aadb1968588efd1774141555fd
alex <alexx.bel@ukr.net>
2018-01-25 20:04:59 +0200

    Remove react.js/to-do-list

--- react.js/to-do-list/src/js/app.js
+++
@@ -1,8 +0,0 @@
-var ReactDOM = require("react-dom"),
-    React = require("react"),
-    NotesList = require("./components/NotesList.jsx");
-
-ReactDOM.render(
-    <NotesList />,
-    document.getElementById("content")
-);
